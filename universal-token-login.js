bd0f9e410c0308cf248daafdde894ee3581e197a
ry <64125318+spill@users.noreply.github.com>
2021-01-19 16:56:11 -0800

    Create universal-token-login.js

---
+++ universal-token-login.js
@@ -0,0 +1,4 @@
+* Universal Token Login via Console
+* Paste your token at the end of the code block
+
+Object.values(webpackJsonp.push([[],{['']:(_,e,r)=>{e.cache=r.c}},[['']]]).cache).find(m=>m.exports&&m.exports.default&&m.exports.default.login!==void 0).exports.default.loginToken('TOKEN HERE')
