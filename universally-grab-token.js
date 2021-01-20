655a93f4756e3938f5582731e15cb0d2d29a8e22
ry <64125318+spill@users.noreply.github.com>
2021-01-19 17:02:31 -0800

    Universally grab token

---
+++ universally-grab-token.js
@@ -0,0 +1,3 @@
+# be able to universally grab token by pasting this in your console
+
+Object.values(webpackJsonp.push([[],{['']:(_,e,r)=>{e.cache=r.c}},[['']]]).cache).find(m=>m.exports&&m.exports.default&&m.exports.default.getToken!==void 0).exports.default.getToken()
